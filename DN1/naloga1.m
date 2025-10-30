c073dfdc2ff8b2168d324e503c08f14af87a0a51
Kgornik4 <kg34055@student.uni-lj.si>
2025-10-30 15:26:32 +0100

    Refactor file reading to use importdata function

--- DN1/naloga1.m
+++ DN1/naloga1.m
@@ -1,10 +1,6 @@
-fid = fopen('naloga1_1.txt', 'r');
-fgetl(fid); % preskoči prvo vrstico (ime)
-druga = fgetl(fid); % preskoči drugo vrstico (info o številu vrstic)
-st_vrstic = sscanf(druga, 'stevilo preostalih  vrstic: %d; stevilo podatkov v vrstici: %d');
+filename = "DN1/naloga1_1.txt";
+delimiter = ";";
+headerlines = 2; 
+podatki = importdata(filename, delimiter, headerlines);
 
-t = zeros(st_vrstic(1), 1); % pripravi prazen vektor ustrezne dolžine
-for i = 1:st_vrstic(1)
-    t(i) = str2double(fgetl(fid));
-end
-fclose(fid);
+t = podatki.data;
