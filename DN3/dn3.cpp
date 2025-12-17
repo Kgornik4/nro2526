68808114844b9d7c102e2488def3a3d66a982664
Kgornik4 <kg34055@student.uni-lj.si>
2025-12-17 15:01:41 +0100

    Update and rename dn2.cpp to dn3.cpp

--- DN3/dn3.cpp
+++ DN3/dn3.cpp
@@ -2,24 +2,18 @@
 #include <cmath>
 
 using namespace std;
-
-// konstanta pi (ker M_PI v Visual Studiu ni definirana)
 const double PI = 3.14159265358979323846;
 
-// Taylorjev razvoj za arctan(x), |x| < 1
 double calctan(double* x, int* N_steps)
 {
     double sum = 0.0;
-
     for (int n = 0; n < *N_steps; n++)
     {
         sum += pow(-1, n) * pow(*x, 2 * n + 1) / (2 * n + 1);
     }
-
     return sum;
 }
 
-// f(x) = e^(3x) * arctan(x/2)
 double f(double x, int N_steps)
 {
     double arg = x / 2.0;
@@ -28,18 +22,15 @@ double f(double x, int N_steps)
 
 int main()
 {
-    // meje integracije
     double a = 0.0;
     double b = PI / 4.0;
 
-    // parametri
-    int n = 1000;        // število trapezov
-    int N_steps = 50;    // število členov Taylorjeve vrste
+    int n = 1000;      
+    int N_steps = 50;   
 
     double h = (b - a) / n;
     double integral = 0.0;
 
-    // trapezna metoda
     integral += f(a, N_steps);
     integral += f(b, N_steps);
 
@@ -50,9 +41,6 @@ int main()
     }
 
     integral *= h / 2.0;
-
-    // izpis rezultata
     cout << "Priblizek integrala je: " << integral << endl;
-
     return 0;
 }
